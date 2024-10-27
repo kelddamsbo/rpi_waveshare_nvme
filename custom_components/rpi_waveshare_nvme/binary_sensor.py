da3a0b9da659b6df1f7619ca0e7cd26edb9d2ec4
kelddamsbo <121604835+kelddamsbo@users.noreply.github.com>
2024-10-27 15:01:37 +0100

    Update binary_sensor.py

--- custom_components/rpi_waveshare_nvme/binary_sensor.py
+++ custom_components/rpi_waveshare_nvme/binary_sensor.py
@@ -14,24 +14,24 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
-from . import NVME, NVMEEntity
+from . import UPS, UPSEntity
 from .const import CONF_COORDINATOR, CONF_MIN_CHARGING, DEF_MIN_CHARGING, DOMAIN
 
 # endregion
 
 
 @dataclass
-class NVMEBinarySensorDescriptionMixin:
+class UPSBinarySensorDescriptionMixin:
     """Additional attributes of the binary sensor description."""
 
-    value_fn: Callable[[NVME], bool]
+    value_fn: Callable[[UPS], bool]
 
 
 @dataclass
-class NVMEBinarySensorEntityDescription(
+class UPSBinarySensorEntityDescription(
     BinarySensorEntityDescription, UPSBinarySensorDescriptionMixin
 ):
-    """Describes NVME binary sensor entity."""
+    """Describes UPS binary sensor entity."""
 
 
 async def async_setup_entry(
@@ -42,11 +42,11 @@ async def async_setup_entry(
     """Create the binary sensor entities."""
     coordinator: DataUpdateCoordinator = hass.data[DOMAIN][CONF_COORDINATOR]
 
-    binary_sensors: list[NVMEBinarySensorEntity] = [
-        NVMEBinarySensorEntity(
+    binary_sensors: list[UPSBinarySensorEntity] = [
+        UPSBinarySensorEntity(
             config_entry=config_entry,
             coordinator=coordinator,
-            description=NVMEBinarySensorEntityDescription(
+            description=UPSBinarySensorEntityDescription(
                 device_class=BinarySensorDeviceClass.BATTERY_CHARGING,
                 key="battery_state",
                 name="Battery State",
@@ -60,13 +60,13 @@ async def async_setup_entry(
     async_add_entities(binary_sensors, update_before_add=True)
 
 
-class NVMEBinarySensorEntity(NVMEEntity, BinarySensorEntity):
-    """Representation of a NVME binary sensor."""
+class UPSBinarySensorEntity(UPSEntity, BinarySensorEntity):
+    """Representation of a UPS binary sensor."""
 
     def __init__(
         self,
         coordinator: DataUpdateCoordinator,
-        description: NVMEBinarySensorEntityDescription,
+        description: UPSBinarySensorEntityDescription,
         config_entry: ConfigEntry,
     ) -> None:
         """Initialise."""
